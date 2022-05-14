1ed0c42474409935db921113d2e9305aaab1086e
Eduardo Ismael <esiespinosa@gmail.com>
2022-05-14 18:08:15 -0500

    commit dos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 
 import { ContadorMudule } from './contador/contador.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorMudule
+    ContadorMudule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styleUrls: ['./agregar.component.css']
+})
+export class AgregarComponent{
+
+  @Input() nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  };
+
+  //@Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+  constructor( private dbzService: DbzService ) { }
+
+  agregar(){
+
+    if (this.nuevo.nombre.trim().length === 0){ return; }
+    if (this.nuevo.poder === 0){ return; }
+
+    //this.onNuevoPersonaje.emit(this.nuevo);  -> Manda lo que tiene el los input al main component
+    this.dbzService.agregarPersonaje( this.nuevo);
+
+    this.nuevo = {nombre: '', poder: 0};
+
+  }
+
+}
